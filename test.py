d93590c2b4366143122dea8a5a0f3aa844378df1
BraveStrife <auemalaw@gmail.com>
2021-08-07 16:19:15 +0700

    TestDesktopGui

--- test.py
+++ test.py
@@ -1,4 +1,20 @@
-print("test")
+import tkinter as tk
 
-sum = 5 * 10
-print(sum)
+def set_message():
+    text = text_input.get()
+    title_label.configure(text=text)
+
+window = tk.Tk()
+window.title('Python')
+window.minsize(width=400, height=400)
+
+title_label = tk.Label(master=window, text='test')
+title_label.pack()
+
+text_input = tk.Entry(master=window)
+text_input.pack()
+
+ok_button = tk.Button(master=window, text='OK', command=set_message)
+ok_button.pack()
+
+window.mainloop()
